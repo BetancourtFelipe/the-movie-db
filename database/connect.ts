08f3cf7750fc6af6b5846c3a6fe6a3bd01904bfe
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-11-25 18:57:55 +0100

    Update connect.ts

--- database/connect.ts
+++ database/connect.ts
@@ -1,1 +1,11 @@
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
 
+config();
+
+export const sql = postgres({
+  transform: {
+    ...postgres.camel,
+    undefined: null,
+  },
+});
