3b51169fdfa46dc01061d6bc9b092f2aaf031eab
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-11-27 18:20:41 +0100

    Update createTableUsers.ts

--- migrations/createTableUsers.ts
+++ migrations/createTableUsers.ts
@@ -5,3 +5,14 @@ export type User = {
   username: string;
   // Omit passwordHash for security
 };
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE
+      users (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        username VARCHAR(100) NOT NULL,
+        password_hash VARCHAR(100) NOT NULL
+      )
+  `;
+}
