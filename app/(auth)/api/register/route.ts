a48e15b6f1af6f3fe7b1f6011802183dd5db0b90
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-11-27 18:28:32 +0100

    Update route.ts

--- app/(auth)/api/register/route.ts
+++ app/(auth)/api/register/route.ts
@@ -2,7 +2,7 @@
 // import { cookies } from 'next/headers';
 // import { createSession } from '../../../../database/sessions';
 // import { createUser, getUserByUsername } from '../../../../database/users';
-// import { User } from '../../../../migrations/1687334782-createUsers';
+// import { User } from '../../../../migrations/createTableUsers.ts';
 // import { secureCookieOptions } from '../../../../util/cookies';
 
 import bcrypt from 'bcrypt';
